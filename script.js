d8a393bbb8dd034dd2f0c1931d0acd79d3e73661
Yurrr00 <yuri.andreasyan@tumo.org>
2022-07-12 18:29:04 +0400

    second commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const btnStart = document.querySelector(".btnStart");
+const modalWindow = document.querySelector(".modal");
+const newGameBtn = document.querySelector(".modal .input_btn");
+const input = document.querySelector(".modal .inputcell")
+const gameArea = document.querySelector(".game")
+const title = document.querySelector(".area .game .title")
+const controlBtn = document.querySelector(".control")
+const restartBtn = document.querySelector(".btn_restart")
+const quitGame = document.querySelector(".btn_quit")
+const winnerWindow = document.querySelector(".winner")
+const winnerText = document.querySelector(".winner h2")
+
